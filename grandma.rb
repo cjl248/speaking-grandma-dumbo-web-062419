1eae0449b1e345b2113600e6201d00bf6b346ed7
Chris Liendo <cjl248@cornell.edu>
2019-06-20 03:33:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,12 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return ""
+    return "I LOVE YOU TOO PUMPKIN!"
+    
   else if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else if phrase != phrase.upcase
+    
+  else
     return "HUH?! SPEAK UP, SONNY!"
+  end
+end
